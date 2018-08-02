d847883e35bf29546a80a9ddbc29a1e84cd0f246
eliranshani <eliran.shani@gmail.com>
2018-08-03 00:54:43 +0300

    login via instagram

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+    $settings = array(
+        "clientID" => "9d31f652d0bb423e9816d63ad93a7c3d",
+        "clientSecret" => "e0bb05ed43ac42ddad27ff58b5c9f782",
+        "redirectURI" => "http://localhost:8888/callback.php"
+    );
+?>
